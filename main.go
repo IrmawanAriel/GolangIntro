3baf5a140a5a6e91c16f970637478f68a4838a06
arielirmawan <arielirmawan@gmail.com>
2024-07-26 11:34:50 +0700

    task 1

--- main.go
+++ main.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main() {
-	fmt.Println("test")
+	segitiga(50)
+}
+
+func segitiga(x int) {
+
+	for j := x; j >= 0; j-- {
+
+		for k := 0; k <= x-j; k++ {
+			fmt.Print(" ")
+		}
+		for i := (2 * j) - 1; i >= 1; i-- {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+
 }
